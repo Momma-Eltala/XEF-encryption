411a963c47e691bb39f54a10567a55068bcedfdb
Jonathan (Eltala) Moore <93369519+Momma-Eltala@users.noreply.github.com>
2024-05-05 00:53:55 -0500

    Update filemanager.asm

--- src/filemanager.asm
+++ src/filemanager.asm
@@ -5,6 +5,9 @@ filedescriptor1 resb 8
 filedescriptor2 resb 8
 fileheader resb 4
 file1size resb 8
+offsetoffile resb 8
+tempo1 resb 8
+tempo2 resb 8
 section .data
 askownertext db " is a file that exist. would you like to write over it? (N/or y) : ", 0x00
 FDNXS db " Does not exist.", 0x00
@@ -12,6 +15,8 @@ fileexists db "Opened file < ", 0x00
 ipfs db "Input file size is = ", 0x00
 bytessize db " bytes", 0x00
 exitbecausenotpermited db "Stopping program because not permited to overwrite ", 0x00
+header db 0x01, "XEF"
+filer db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 section .text
 
 file_open:
@@ -108,6 +113,7 @@ file_open:
 		mov rsi, 0
 		mov rdx, 0
 		syscall
+		call headermake
 
 		ret
 
@@ -144,3 +150,67 @@ file_open:
 				mov rax, 60
 				mov rdi, 0
 				syscall
+				
+
+filewrite:
+	;RAX | will be used to store where we will write, the offset of the file
+	;RDI | string location
+	;RDX | how how many bytes to write, if 0 will find out on its own will add a null at the end
+	;This is a call function
+
+	mov qword [offsetoffile], rax
+	mov qword [tempo1], rdi
+	mov qword [tempo2], rdx
+	
+	mov rax, 8
+	mov rdi, qword [filedescriptor2]
+	mov rsi, qword [offsetoffile]
+	mov rdx, 0
+	syscall
+
+	mov rax, qword [tempo2]
+	cmp rax, 0
+	jne ignorefindinglength
+	call findlengthofstringforXEF
+	
+	ignorefindinglength:
+	mov rax, 1
+	mov rdi, qword [filedescriptor2]
+	mov rsi, qword [tempo1]
+	mov rdx, qword [tempo2]
+	syscall
+	
+	ret
+headermake:
+	mov rax, 1
+	mov rdi, 4
+	mov rsi, header 
+	mov rdx, 4
+	syscall
+
+	mov rax, 8
+	mov rdi, qword [filedescriptor2]
+	mov rsi, 0
+	mov rdx, 0
+	syscall
+
+	ret
+
+findlengthofstringforXEF:
+	xor rcx, rcx
+	mov cl, 0
+	xor rdx, rdx
+	mov rax, qword [tempo1]
+
+	findlengthofstringforXEFloop:
+
+	cmp cl, byte [rax]
+	je findlengthofstringforXEFdone
+	inc rax
+	inc rdx
+	jmp findlengthofstringforXEFloop 
+
+	findlengthofstringforXEFdone:
+	inc rdx
+	mov qword [tempo2], rdx
+	ret
